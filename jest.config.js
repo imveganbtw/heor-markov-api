844cb34c5de18ff016e31b18b1f1394edcbe8e9a
Samantha Quiñones <samantha@skillshare.com>
2022-04-27 16:46:50 -0400

    coverage reporting

--- jest.config.js
+++ jest.config.js
@@ -3,5 +3,5 @@ module.exports = {
   preset: "ts-jest",
   testEnvironment: "node",
   testPathIgnorePatterns: ["node_modules", "prod_node_modules"],
-  reporters: ["default", "jest-junit"],
+  coverageReporters: ["text-summary", "cobertura"],
 };
